84f2d8a20afe448ab2be49a20decb6a184af113d
Diyorbektuit <d.xazratqulov@tuit.uz>
2025-05-07 23:36:38 +0500

    updates

--- core/settings/production.py
+++ core/settings/production.py
@@ -2,11 +2,11 @@ from .base import *  # noqa
 
 DEBUG = True
 
-CSRF_COOKIE_SECURE = True
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:8000",
-    "https://admin.kassalite.uz"
-]
+# CSRF_COOKIE_SECURE = True
+# CSRF_TRUSTED_ORIGINS = [
+#     "http://localhost:8000",
+#     "https://admin.kassalite.uz"
+# ]
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
