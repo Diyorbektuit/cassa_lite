c0fea93ced885f9250ea557a60a4676098cf16e2
Diyorbektuit <d.xazratqulov@tuit.uz>
2025-05-17 10:05:44 +0500

    updates

--- core/settings/production.py
+++ core/settings/production.py
@@ -2,12 +2,26 @@ from .base import *  # noqa
 
 DEBUG = True
 
-# CSRF_COOKIE_SECURE = True
-# CSRF_TRUSTED_ORIGINS = [
-#     "http://localhost:8000",
-#     "https://admin.kassalite.uz"
-# ]
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://localhost:5174",
+    "http://localhost:5173",
+    "https://admin.kassalite.uz",
+    "https://kassalite.uz",
+    "https://account.kassalite.uz"
+]
 
-CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = ["*"]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://localhost:5174",
+    "http://localhost:5173",
+    "https://admin.kassalite.uz",
+    "https://kassalite.uz",
+    "https://account.kassalite.uz"
+]
